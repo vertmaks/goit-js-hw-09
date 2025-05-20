bb37a7569747e8ae34ef8f71bed28bfcdee745da
Maksym Veretnov <maxvertnov@gmail.com>
2025-05-21 00:21:48 +0200

    feat: enhance form functionality; add local storage handling, improve error message management, and refactor event listeners

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,57 @@
 import { emailCheck, msgCheck, formSubmit } from './form-check-logic';
 import { refs } from './refs';
 
-refs.emailInput.addEventListener('blur', emailCheck);
-refs.textInput.addEventListener('blur', msgCheck);
-refs.form.addEventListener('submit', formSubmit);
+const STORAGE_KEY = 'feedback-form-state';
+const form = refs.form;
+const emailInput = refs.emailInput;
+const textInput = refs.textInput;
+
+refreshInputValue();
+
+emailInput.addEventListener('blur', emailCheck);
+
+textInput.addEventListener('blur', msgCheck);
+
+form.addEventListener('submit', formSubmit);
+
+form.addEventListener('submit', handleFormSubmit);
+
+form.addEventListener('focusout', handleFormInput);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.reset();
+}
+
+function handleFormInput(event) {
+  const form = event.currentTarget;
+  const formData = new FormData(form);
+  const formDataObj = Object.fromEntries(formData.entries());
+  console.log(' handleFormInput res:', formDataObj);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataObj));
+}
+
+function refreshInputValue() {
+  const savedLSData = localStorage.getItem(STORAGE_KEY);
+
+  if (!savedLSData) return;
+
+  try {
+    const dataFromLS = JSON.parse(savedLSData);
+
+    const formEl = new FormData(form);
+    const formFields = Array.from(formEl.keys());
+    console.log(' refreshInputValue formFields:', formFields);
+
+    formFields.forEach(field => {
+      form.elements[field].value = dataFromLS[field];
+    });
+  } catch (error) {
+    alert('Ooops... something went wrong');
+  }
+}
--- src/js/form-check-logic.js
+++ src/js/form-check-logic.js
@@ -3,83 +3,59 @@ import { refs } from './refs';
 
 let msgTimeoutId = null;
 let emailTimeoutId = null;
+let shakeId = null;
 
 export function emailCheck() {
-  const correctEmailRule = emailInput.value.match(
+  const correctEmailRule = refs.emailInput.value.match(
     /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
   );
 
-  const existingError = refs.emailInput.parentElement.querySelector(
-    '.form-email-error-msg'
-  );
-  if (existingError) existingError.remove();
-
   if (refs.emailInput.value === '') {
-    refs.emailInput.style.borderColor = 'tomato';
-
-    //   insert empty error to HTML!
-    refs.emailInput.insertAdjacentHTML('afterend', emptyErrorMsg);
-    const emptyMsg = refs.emailInput.parentElement.querySelector(
+    const existingError = refs.emailInput.parentElement.querySelector(
       '.form-error-empty-msg'
     );
-    errorMsg.classList.add('is-onscreen');
+    if (existingError) existingError.remove();
 
-    // old example
-    // emailEmptyError.classList.remove('visually-hidden');
-    // emailEmptyError.classList.add('is-onscreen');
-    // emailErrorMsg.classList.remove('is-onscreen');
+    refs.emailInput.style.borderColor = 'tomato';
+    refs.emailInput.insertAdjacentHTML('afterend', emptyErrorMsg);
+
+    const emptyError = refs.emailInput.parentElement.querySelector(
+      '.form-error-empty-msg'
+    );
+    emptyError.classList.add('is-onscreen');
 
     if (emailTimeoutId) clearTimeout(emailTimeoutId);
     emailTimeoutId = setTimeout(() => {
       refs.emailInput.style.borderColor = '';
-
-      // remove empty error from HTML!
-      emptyMsg.classList.remove('is-onscreen');
-      emptyMsg.remove();
-
-      // old example
-      //   emailEmptyError.classList.remove('is-onscreen');
-      //   emailEmptyError.classList.add('visually-hidden');
+      emptyError.classList.remove('is-onscreen');
+      emptyError.remove();
     }, 5000);
 
     return false;
   } else if (!correctEmailRule) {
-    refs.emailInput.style.borderColor = 'tomato';
-
-    //   insert ivalid email error to HTML
-    refs.emailInput.insertAdjacentHTML('afterend', emailErrorMsg);
-    const errorMsg = refs.emailInput.parentElement.querySelector(
+    const existingError = refs.emailInput.parentElement.querySelector(
       '.form-email-error-msg'
     );
-    errorMsg.classList.add('is-onscreen');
+    if (existingError) existingError.remove();
 
-    //   old example
-    // emailErrorMsg.classList.remove('visually-hidden');
-    // emailErrorMsg.classList.add('is-onscreen');
-    // emailEmptyError.classList.remove('is-onscreen');
+    refs.emailInput.style.borderColor = 'tomato';
+    refs.emailInput.insertAdjacentHTML('afterend', emailErrorMsg);
+
+    const emailError = refs.emailInput.parentElement.querySelector(
+      '.form-email-error-msg'
+    );
+    emailError.classList.add('is-onscreen');
 
     if (emailTimeoutId) clearTimeout(emailTimeoutId);
     emailTimeoutId = setTimeout(() => {
       refs.emailInput.style.borderColor = '';
-
-      // remove ivalid email error from HTML
-      errorMsg.classList.remove('is-onscreen');
-      errorMsg.remove();
-
-      // old example
-      //   emailErrorMsg.classList.remove('is-onscreen');
-      //   emailErrorMsg.classList.add('visually-hidden');
+      emailError.classList.remove('is-onscreen');
+      emailError.remove();
     }, 5000);
 
     return false;
   } else {
-    emailInput.style.borderColor = '';
-
-    //   don't add enything!
-    //   old!
-    // emailErrorMsg.classList.remove('is-onscreen');
-    // emailEmptyError.classList.remove('is-onscreen');
-
+    refs.emailInput.style.borderColor = '';
     if (emailTimeoutId) clearTimeout(emailTimeoutId);
 
     return true;
@@ -87,46 +63,30 @@ export function emailCheck() {
 }
 
 export function msgCheck() {
-  const existingError = refs.textInput.parentElement.querySelector(
-    '.form-error-empty-msg'
-  );
-  if (existingError) existingError.remove();
-
   if (refs.textInput.value.trim() === '') {
-    refs.textInput.style.borderColor = 'tomato';
-
-    //   insert empty HTML error!
-    refs.textInput.insertAdjacentHTML('afterend', emptyErrorMsg);
-    const emptyMsg = refs.textInput.parentElement.querySelector(
+    const existingError = refs.textInput.parentElement.querySelector(
       '.form-error-empty-msg'
     );
-    emptyMsg.classList.add('is-onscreen');
+    if (existingError) existingError.remove();
 
-    //   old
-    // nameEmptyError.classList.remove('visually-hidden');
-    // nameEmptyError.classList.add('is-onscreen');
+    refs.textInput.style.borderColor = 'tomato';
+    refs.textInput.insertAdjacentHTML('afterend', emptyErrorMsg);
+
+    const emptyError = refs.textInput.parentElement.querySelector(
+      '.form-error-empty-msg'
+    );
+    emptyError.classList.add('is-onscreen');
 
     if (msgTimeoutId) clearTimeout(msgTimeoutId);
     msgTimeoutId = setTimeout(() => {
       refs.textInput.style.borderColor = '';
-
-      // remove empty HTML error!
-      emptyMsg.classList.remove('is-onscreen');
-      emptyMsg.remove();
-
-      // old
-      //   nameEmptyError.classList.remove('is-onscreen');
+      emptyError.classList.remove('is-onscreen');
+      emptyError.remove();
     }, 5000);
 
     return false;
   } else {
     refs.textInput.style.borderColor = '';
-
-    //   remove empty HTML error!
-
-    //   nameEmptyError.classList.remove('is-onscreen');
-    // nameEmptyError.classList.add('visually-hidden');
-
     if (msgTimeoutId) clearTimeout(msgTimeoutId);
 
     return true;
@@ -135,23 +95,75 @@ export function msgCheck() {
 
 export function formSubmit(event) {
   event.preventDefault();
-  const form = event.target;
+  const form = refs.form;
   const isMsgFilled = msgCheck();
   const isEmailValid = emailCheck();
 
   if (!isEmailValid) {
     if (refs.emailInput.value === '') {
-      triggerShake(refs.emptyErrorMsg);
+      const existingError = refs.emailInput.parentElement.querySelector(
+        '.form-error-empty-msg'
+      );
+      if (existingError) existingError.remove();
+      refs.emailInput.insertAdjacentHTML('afterend', emptyErrorMsg);
+      waitForElement(
+        '.form-error-empty-msg',
+        refs.emailInput.parentElement,
+        shakeError => {
+          triggerShake(shakeError);
+        }
+      );
     } else {
-      triggerShake(refs.emailErrorMsg);
+      const existingError = refs.emailInput.parentElement.querySelector(
+        '.form-email-error-msg'
+      );
+      if (existingError) existingError.remove();
+      refs.emailInput.insertAdjacentHTML('afterend', emailErrorMsg);
+      waitForElement(
+        '.form-email-error-msg',
+        refs.emailInput.parentElement,
+        shakeError => {
+          triggerShake(shakeError);
+        }
+      );
     }
   }
 
   if (!isMsgFilled) {
-    triggerShake(refs.emptyErrorMsg);
+    const existingError = refs.textInput.parentElement.querySelector(
+      '.form-error-empty-msg'
+    );
+    if (existingError) existingError.remove();
+    refs.textInput.insertAdjacentHTML('afterend', emptyErrorMsg);
+    waitForElement(
+      '.form-error-empty-msg',
+      refs.textInput.parentElement,
+      shakeError => {
+        triggerShake(shakeError);
+      }
+    );
+
+    if (shakeId) clearTimeout(shakeId);
+    shakeId = setTimeout(() => {
+      refs.textInput.parentElement
+        .querySelector('.form-error-empty-msg')
+        .remove();
+    }, 5000);
   }
 
   form.reset();
+}
+
+function waitForElement(selector, parent, callback) {
+  const observer = new MutationObserver((mutationsList, observer) => {
+    const element = parent.querySelector(selector);
+    if (element) {
+      observer.disconnect();
+      callback(element);
+    }
+  });
+
+  observer.observe(parent, { childList: true, subtree: true });
 }
 
 export function triggerShake(el) {
